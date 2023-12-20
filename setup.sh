a77f6842b04be4e28ff4268cacb674c1c046a024
ap-19927 <allmannpatrick@gmail.com>
2023-12-20 04:45:57 +0000

    docker nginx certbot

---
+++ setup.sh
@@ -0,0 +1,53 @@
+mkdir data
+mkdir data/nginx
+mkdir data/nginx/templates
+
+cat > data/nginx/templates/app.conf.template << EOF
+server {
+  server_name \${IP} \${URI1};
+  location / {
+    return 301 https://\$host\$request_uri;
+  }
+  location /.well-known/acme-challenge/ {
+    root /var/www/certbot;
+  }
+}
+server {
+  server_name \${URI2};
+  location / {
+    return 301 https://\$host\$request_uri;
+  }
+  location /.well-known/acme-challenge/ {
+    root /var/www/certbot;
+  }
+}
+EOF
+bash letsencrypt-init.sh
+
+cat >> data/nginx/templates/app.conf.template << EOF
+server {
+  listen \${SSL_PORT} ssl;
+  server_name \${URI1};
+  location / {
+    proxy_pass http://\${PROXY_IP}:\${PORT1};
+  }
+  ssl_certificate /etc/letsencrypt/live/\${URI}-0001/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/\${URI}-0001/privkey.pem;
+  include /etc/letsencrypt/options-ssl-nginx.conf;
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+  client_max_body_size 10M;
+}
+server {
+  listen \${SSL_PORT} ssl;
+  server_name \${URI2};
+  location / {
+    proxy_pass http://\${PROXY_IP}:\${PORT2};
+  }
+  ssl_certificate /etc/letsencrypt/live/\${URI}-0001/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/\${URI}-0001/privkey.pem;
+  include /etc/letsencrypt/options-ssl-nginx.conf;
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+  client_max_body_size 10M;
+}
+EOF
+bash letsencrypt-init.sh
